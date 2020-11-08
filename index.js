9e81efec10f3d447a1c83566c12d880143c9df2d
Gautier78 <Gautier-lemaitre@hotmail.com>
2020-11-08 03:09:54 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+var firstName = "Gautier";
+
+var  favouriteColor = "blue";
+
+var favouriteFood = "Burito";
+
+var city = "Paris";
+
+/*
+var x = "tic";
+
+x = "tac";
+
+x = "toe";
+
+console.log(x);
+*/
+
+/*
+var x = 'Laurel';
+
+var y = 'Hardy';
+
+var z = y;
+
+y = x;
+
+x = z;
+
+console.log(y);
+*/
+
+// console.log display the variables in the console for firstName, favouriteColor, favouriteFood and city. 
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood,",",city);
+
+var phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+var endingPhrase = " is what we see. Rainbow colours, are made with light bending you see!";
+var one = "Red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indigo";
+var seven = "violet";
+
+console.log(phrase , one + "," , two + ",", three + ",", four + ",", five + ",", six + ",", seven + endingPhrase);
+
+/*
+“A rainbow is formed by the sunlight. Shining through rain drops so small. The
+rain bends the light from the sunshine, and colors the sky for us all. Red, orange,
+yellow, green, blue, indigo, violet is what we see. Rainbow colours, are made
+with light bending you see!”
+*/
+
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+
+function myAlphabetLength(){
+    console.log(myAlphabet);
+};
+
+myAlphabetLength();
+
+var x = 5, y = 10, z = 15;
+/*
+x + y;
+
+y - x;
+
+x * y;
+
+y / x;
+
+x % 2;
+
+x ++;
+
+x --;
+*/
+
